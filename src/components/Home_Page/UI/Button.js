464e3a3611e6a99fc1411d2e5538e33e8e54d622
kXrock <keremkaya08@hotmail.com>
2023-11-27 01:47:42 +0300

    added style props

--- src/components/Home_Page/UI/Button.js
+++ src/components/Home_Page/UI/Button.js
@@ -4,7 +4,7 @@ import classes from "./Button.module.css";
 
 
 const Button = props => {
-    return (<button className={` ${classes.button} ${props.className}`} type="submit" onClick={props.onClick}>
+    return (<button className={` ${classes.button} ${props.className}`} type="submit" onClick={props.onClick} style={props.style}>
             {props.children}
         </button>);
 
